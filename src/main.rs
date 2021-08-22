7ea60cd1d97b0953961292bbd81c53ee098e0e4c
quiarom <userquiarom@gmail.com>
2021-08-22 00:47:09 -0400

    Validación de la respuesta en el servidor

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,20 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1056];
     stream.read(&mut buffer).unwrap();
 
-    let contents = fs::read_to_string("html/index.html").unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
 
-    let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-        contents.len(),
-        contents
-    );
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap(); 
+    if buffer.starts_with(get) {
+        let contents = fs::read_to_string("html/index.html").unwrap();
+        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap()
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+        let contents = fs::read_to_string("html/404.html").unwrap();
+
+        let response = format!("{}{}", status_line, contents);
+
+        stream.write(response.as_bytes()).unwrap();
+    	stream.flush().unwrap();
+    }
 }
