e92fa8ab0af446ba6558970eb037a3b514ca565a
quiarom <userquiarom@gmail.com>
2021-08-21 21:35:10 -0400

    Conexión HTTP

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
-use std::net::{TcpStream, TcpListener};
+use std::fs;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
-    
     let listener = TcpListener::bind("127.0.0.1:7373").unwrap();
-
     for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                println!("¡Una visita!");
-            } Err(e) => {
-                println!("Conexión fallida, F");
-            }
-        }
+        let stream = stream.unwrap();
+        handle_connection(stream);
     }
+}
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1056];
+    stream.read(&mut buffer).unwrap();
 
+    let contents = fs::read_to_string("html/index.html").unwrap();
+
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap(); 
 }
